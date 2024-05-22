99a5199a056879026df09f6ea5bd0a2074fd7953
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-05-22 10:53:55 +0100

    Updated HTML structure in index.php

---
+++ app/public/wp-content/themes/Own/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <?php wp_head(); ?>
+   
+</head>
+
+<body <?php body_class('example'); ?>>
+<?php wp_body_open(); ?>
+    <p>hello</p>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
